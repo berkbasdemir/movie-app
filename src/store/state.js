93c300cd10f1323d27593286db771dc516411e74
berkbasdemir <basdemirberk@gmail.com>
2023-11-07 01:06:08 +0300

    Home Page Part 1 is done.

---
+++ src/store/state.js
@@ -0,0 +1,10 @@
+export const state = {
+    hoveredElement: null,
+    subcategories: {
+        movies: ['Action', 'Drama', 'Comedy'],
+        tvshows: ['Sci-Fi', 'Drama', 'Comedy'],
+        people: ['Actors', 'Directors', 'Producers'],
+        more: ['Category1', 'Category2', 'Category3'],
+    },
+    movies: [],
+};
